884f6c3b024aae4d2d1c2fdfe2e4a0d231724640
Elliott Asher <elliott250@gmail.com>
2015-04-26 14:24:51 -0400

    moved war specific methods to the class war

--- Code/PlayWar.java
+++ Code/PlayWar.java
@@ -16,29 +16,14 @@ public class PlayWar
 class MyFrame extends JFrame 
 {
    private JPanel p1,p2,p3,p4,p5,p6;
-   Player player1;
-   Player player2;
-   boolean roundWinner = false;
-   boolean war = false;
-  
+   War war;
+
    public MyFrame(String s) 
    {
   
       super(s);
       //initiate card game
-      Deck deck = new Deck("../cardpics");
-      player1 = new Player();
-      player2 = new Player();
-      
-      //deal cards to players
-      int count = 0;
-      while(!deck.isEmpty())
-      {
-         if(++count % 2 == 0)
-            player1.addCard(deck.deal());
-         else
-            player2.addCard(deck.deal()); 
-      }
+      this.war = new War();      
 
       //creat buttons
       ImageIcon image = new ImageIcon("../cardpics/back.jpg");
@@ -56,8 +41,8 @@ class MyFrame extends JFrame
       p1 = new JPanel();
       p1.setLayout(new GridLayout(3,1));
       p1.add(new JLabel("Player 1"));
-      p1.add(player1.getWinnerTextLabel());
-      p1.add(player1.getCardsInHandLabel());
+      p1.add(this.war.getPlayer1().getWinnerTextLabel());
+      p1.add(this.war.getPlayer1().getCardsInHandLabel());
       p1.setBackground(Color.GREEN);
 
       p2 = new JPanel();
@@ -66,14 +51,14 @@ class MyFrame extends JFrame
       p2.setBackground(Color.GREEN);
 
       p3 = new JPanel();
-      p3.add(player1.getLiveCardImage());
+      p3.add(this.war.getPlayer1().getLiveCardImage());
       p3.setBackground(Color.GREEN);
 
       p4 = new JPanel();
       p4.setLayout(new GridLayout(3,1));
       p4.add(new JLabel("Player 2"));
-      p4.add(player2.getWinnerTextLabel());
-      p4.add(player2.getCardsInHandLabel());
+      p4.add(this.war.getPlayer2().getWinnerTextLabel());
+      p4.add(this.war.getPlayer2().getCardsInHandLabel());
       p4.setBackground(Color.GREEN);
 
       p5 = new JPanel();
@@ -82,7 +67,7 @@ class MyFrame extends JFrame
       p5.setBackground(Color.GREEN);
 
       p6 = new JPanel();
-      p6.add(player2.getLiveCardImage());
+      p6.add(this.war.getPlayer2().getLiveCardImage());
       p6.setFocusable(false);
       p6.setBackground(Color.GREEN);
 
@@ -95,78 +80,13 @@ class MyFrame extends JFrame
       addKeyListener(new NewRoundListener()); 
       setFocusable(true);
    }
-   
-   public boolean bothPlayed()
-   {
-    return (player1.hasLiveCard() && player2.hasLiveCard()); 
-   }
-   public void determinWar()
-   {
-
-   }
-   public void determineWinner()
-   {
-      this.war = War.determineWar(player1.lookAtTopLiveCard(),player2.lookAtTopLiveCard());
-      if(this.war)
-      {
-        player1.playWar();
-        player2.playWar();
-        this.war = false;
-        roundWinner = false;
-      }
-      else if(War.determineWinner(player1.lookAtTopLiveCard(),player2.lookAtTopLiveCard()) <  0)
-      {
-        System.out.println("h1");
-        player1.setToWinner();
-        player1.takeWinnings(player2);
-
-        player1.setCardsInHand();
-        player2.setCardsInHand();
-        roundWinner = true;
-
-      }
-      else
-      {
-        System.out.println("h2");
-
-        player2.setToWinner();
-        player2.takeWinnings(player1);
-        player1.setCardsInHand();
-        player2.setCardsInHand();
-        roundWinner = true;
-
-      }
-   }
-
-   public void playCard(Player player)
-   {
-      if(!player.isEmpty() && !player.hasLiveCard() && !roundWinner)
-      {
-        player.playCard();
-        player.setLiveCardImage();
-      }
   
-      if(bothPlayed())
-      {
-        System.out.println("hello");
-        determineWinner();
-      }
-  }
   
-  public void pauseScreen(int sec)
-  {
-    try
-    {
-      Thread.sleep(sec);
-    }
-    catch(Exception ex){}
-  }
-
   class Player1Listener implements ActionListener
   {
     public void actionPerformed(ActionEvent e) 
     {
-      playCard(player1);     
+      war.playCard(war.getPlayer1());     
     }
   }
 
@@ -174,7 +94,7 @@ class MyFrame extends JFrame
   {
     public void actionPerformed(ActionEvent e) 
     {
-      playCard(player2);
+      war.playCard(war.getPlayer2());
     }
   }
 
@@ -185,16 +105,17 @@ class MyFrame extends JFrame
   
   public void keyPressed(KeyEvent e)
   {
-    if(e.getKeyChar() == KeyEvent.VK_ENTER && roundWinner)
+    if(e.getKeyChar() == KeyEvent.VK_ENTER && war.roundWinner)
     {
-      player1.removeLiveCardImage();
-      player2.removeLiveCardImage();
-      player1.setWinnerText(null);
-      player2.setWinnerText(null);
-      player1.roundOver(); player2.roundOver();
-      roundWinner = false;
+      war.getPlayer1().removeLiveCardImage();
+      war.getPlayer2().removeLiveCardImage();
+      war.getPlayer1().setWinnerText(null);
+      war.getPlayer2().setWinnerText(null);
+      war.getPlayer1().roundOver(); war.getPlayer2().roundOver();
+      war.roundWinner = false;
     }
   }
+
   public void keyReleased(KeyEvent e){}
   public void keyTyped(KeyEvent e){}
 
--- Code/War.java
+++ Code/War.java
@@ -1,5 +1,39 @@
 public class War
 {
+	Player player1;
+	Player player2;
+	boolean roundWinner = false;
+    boolean war = false;
+
+    public War()
+    {
+      player1 = new Player();
+      player2 = new Player();
+
+      //deal cards to players
+      Deck deck = new Deck("../cardpics");
+
+      int count = 0;
+      while(!deck.isEmpty())
+      {
+         if(++count % 2 == 0)
+            player1.addCard(deck.deal());
+         else
+            player2.addCard(deck.deal()); 
+      }
+
+    }
+
+    public Player getPlayer1()
+    {
+    	return this.player1;
+    }
+
+    public Player getPlayer2()
+    {
+    	return this.player2;
+    }
+
 	public static boolean determineWar(Card card1, Card card2)
 	{
 		return card1.equals(card2);
@@ -12,4 +46,56 @@ public class War
 		else 
 			return 1;
 	}
+
+	public boolean bothPlayed()
+    {
+    	return (player1.hasLiveCard() && player2.hasLiveCard()); 
+    }
+
+   public void determineWinner()
+   {
+      this.war = War.determineWar(player1.lookAtTopLiveCard(),player2.lookAtTopLiveCard());
+      if(this.war)
+      {
+        player1.playWar();
+        player2.playWar();
+        
+        roundWinner = false;
+      }
+      else if(War.determineWinner(player1.lookAtTopLiveCard(),player2.lookAtTopLiveCard()) <  0)
+      {
+        player1.setToWinner();
+        player1.takeWinnings(player2);
+
+        player1.setCardsInHand();
+        player2.setCardsInHand();
+        roundWinner = true;
+
+      }
+      else
+      {
+        player2.setToWinner();
+        player2.takeWinnings(player1);
+        player1.setCardsInHand();
+        player2.setCardsInHand();
+        roundWinner = true;
+
+      }
+   }
+   public void playCard(Player player)
+   {
+      if(!player.isEmpty() && !player.hasLiveCard() && !roundWinner)
+      {
+        player.playCard();
+        player.setLiveCardImage();
+      }
+  
+      if(this.bothPlayed())
+      {
+        this.determineWinner();
+      }
+  	}
+
+
+
 }
